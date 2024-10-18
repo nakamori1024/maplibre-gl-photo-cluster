cd4cd602adabd9fd83426ba78ea786e225a3eb11
nakamori1024 <79179090+nakamori1024@users.noreply.github.com>
2024-10-18 15:30:44 +0900

    Apply transparency to cluster circle

--- src/maplibre-gl-photo-cluster.js
+++ src/maplibre-gl-photo-cluster.js
@@ -34,41 +34,12 @@ export class PhotoExtension {
 
     addClusterLayer = () => {
         this.map.addLayer({
-            id: 'clusters',
+            id: "invisible-points",
             type: 'circle',
             source: 'photos',
             filter: ['has', 'point_count'],
             paint: {
-                'circle-color': [
-                    'step',
-                    ['get', 'point_count'],
-                    '#51bbd6',
-                    100,
-                    '#f1f075',
-                    750,
-                    '#f28cb1'
-                ],
-                'circle-radius': [
-                    'step',
-                    ['get', 'point_count'],
-                    20,
-                    100,
-                    30,
-                    750,
-                    40
-                ]
-            }
-        });
-
-        this.map.addLayer({
-            id: 'cluster-count',
-            type: 'symbol',
-            source: 'photos',
-            filter: ['has', 'point_count'],
-            layout: {
-                'text-field': '{point_count_abbreviated}',
-                'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
-                'text-size': 12
+                'circle-opacity': 0.0,
             }
         });
     }
